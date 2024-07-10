5be3e5584e16aea41d0807112517156f8c52c6b0
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-07-11 00:15:49 +0530

    files

---
+++ src/main/java/com/Yash/Medguardian/Uemail.java
@@ -0,0 +1,14 @@
+package com.Yash.Medguardian;
+
+public class Uemail {
+	public static String uEmail;
+
+	public static String getuEmail() {
+		return uEmail;
+	}
+
+	public static void setuEmail(String uEmail) {
+		Uemail.uEmail = uEmail;
+	}
+
+}
