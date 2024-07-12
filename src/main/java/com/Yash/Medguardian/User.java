72a1acc448419477daebd0711bb8f0c2671d3f41
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-07-13 00:16:23 +0530

    files

---
+++ src/main/java/com/Yash/Medguardian/User.java
@@ -0,0 +1,23 @@
+package com.Yash.Medguardian;
+
+public class User {
+	static double latitude,longitude;
+
+	public static double getLongitude() {
+		return longitude;
+	}
+
+	public static void setLongitude(double longitude) {
+		User.longitude = longitude;
+	}
+
+	public static double getLatitude() {
+		return latitude;
+	}
+
+	public static void setLatitude(double latitude) {
+		User.latitude = latitude;
+	}
+
+
+}
